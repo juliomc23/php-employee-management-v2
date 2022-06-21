71d197715128bd67e730d07133f93d067dec08dd
Oualidromli <oualid.romli@gmail.com>
2022-06-21 12:41:50 +0200

    constant added

---
+++ config/constant.php
@@ -0,0 +1,18 @@
+<?php
+//LIBS
+define("LIBS", BASE_PATH . '/libs');
+
+//CONTROLLERS
+define("CONTROLLERS", BASE_PATH . '/src/controller');
+
+//VIEWS
+define("VIEW", BASE_PATH . '/src/view');
+
+//MODELS
+define("MODELS", BASE_PATH . '/src/models');
+
+//CSS
+define('CSS', BASE_URL . '/public/assets/css');
+
+//JS
+define('JS', BASE_URL . '/public/assets/js');
