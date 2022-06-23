78d2aba373bc110938cb126a4d00ab7a3de9f012
Oualid romli <oualid.romli@gmail.com>
2022-06-23 22:41:49 +0200

    work in prpgress

--- src/view/dashboardView.php
+++ src/view/dashboardView.php
@@ -5,8 +5,18 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <script type="module" src="../assets/js/index.js"></script>
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+  <!-- <link rel="stylesheet" href="../assets/css/login.css"> -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+  <!-- ------------- FOR JS-GRID ------------- -->
+  <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
+  <link type="text/css" rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" />
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
     </style>
@@ -15,7 +25,14 @@
 
 <body>
     <main>
-        <header>
+        <h2>EMPLOYEE MANAGEMENT</h2>
+        <div id="jsGrid" class="pb-4"></div>
+    </main>
+</body>
+<script src="./public/assets/js/main.js"></script>
+</html>
+
+<!-- <header>
             <div id="logo" class="logo__container">
                 <img src="" alt="" />
             </div>
@@ -31,25 +48,4 @@
                 <div>
                 </div>
             </nav>
-        </header>
-        <sectiion class="dashboard__table__containeer">
-            <table id="dashboardEmployeeTable" class="dashboard__employee__table" style="border-spacing: 0px;">
-                <tr id="employeFields" class="dashboard__employe__th">
-                    <th>NAME</th>
-                    <th>EMAIL</th>
-                    <th>AGE</th>
-                    <th>STREET No.</th>
-                    <th>CITY</th>
-                    <th>STATE</th>
-                    <th>POSTAL CODE</th>
-                    <th>PHONE NUMBER</th>
-                    <th>
-                        <button id="dashboardAddNewEmployee">Add Employee</button>
-                    </th>
-                </tr>
-            </table>
-        </sectiion>
-    </main>
-</body>
-
-</html>
+        </header> -->
